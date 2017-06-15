b1dcb28c04d84fbde8a6cd42195f1eb99819d8d8
kripton <kamnev.u1969@gmail.com>
2017-06-15 13:25:14 +0300

    Protocol definitions

---
+++ protocol.h
@@ -0,0 +1,113 @@
+#ifndef PROTOCOL_H_INCLUDED
+#define PROTOCOL_H_INCLUDED
+
+#define PL_CONTROL_PACKETS_MSK         0x0F
+#define PL_PROGRAMMER_PACKETS_MSK      0x1F
+#define PL_UART_PACKETS_MSK            0x2F
+
+/* CONTROL PACKETS */
+#define PL_PROGRAMMER_INIT             0x00
+#define PL_PROGRAMMER_STOP             0x01
+#define PL_UART_INIT                   0x02
+#define PL_UART_STOP                   0x03
+#define PL_RESET                       0x04
+#define PL_ACK_PACKET                  0x05
+#define PL_CLOSE_CONNECTION            0x06
+#define PL_NETWORK_CONFIGURATION       0x07
+#define PL_SET_OBSERVER_KEY            0x08
+#define PL_SET_ENCRYPTION_KEYS         0x09
+#define PL_SET_SIGN_KEYS               0x0A
+#define PL_ENABLE_ENCRYPTION           0x0B
+#define PL_ENABLE_SIGN                 0x0C
+
+/* PROGRAMMER PACKETS */
+#define PL_LOAD_MCU_INFO               0x10
+#define PL_PROGRAM_MEMORY              0x11
+#define PL_READ_MEMORY                 0x12
+#define PL_MEMORY                      0x13
+#define PL_CMD                         0x14
+
+/* UART PACKETS */
+#define PL_UART_CONFIGURATION          0x20
+#define PL_UART_DATA                   0x21
+
+/* Header structure */
+#define PL_START_FRAME_BYTE         0x1B
+
+#define PL_FLAGS_FIELD_OFFSET       1
+#define PL_FLAGS_FIELD_SIZE         1
+
+#define PL_TYPE_FIELD_OFFSET		2
+#define PL_TYPE_FIELD_SIZE          1
+
+#define PL_SIZE_FIELD_OFFSET		3
+#define PL_SIZE_FIELD_SIZE			2
+
+#define PL_PACKET_HEADER_SIZE		(PL_SIZE_FIELD_SIZE + PL_TYPE_FIELD_SIZE + PL_FLAGS_FIELD_SIZE + 1)
+#define PL_PACKET_RESERVED_BYTES	(PL_SIZE_FIELD_SIZE + PL_TYPE_FIELD_SIZE + PL_FLAGS_FIELD_SIZE + 1)
+
+/* Flags byte */
+#define PL_FLAG_COMPRESSION_BIT     0
+#define PL_FLAG_ENCRYPTION_BIT      1
+#define PL_FLAG_SIGN_BIT            2
+
+/* Maximum data field length of packet */
+#define PL_MAX_DATA_LENGTH	        1029
+
+/* ACK packet */
+#define PL_ACK_BYTE_OFFSET          0
+#define PL_ACK_SUCCESS              1
+#define PL_ACK_FAILURE              0
+
+/* Reset packet */
+#define PL_RESET_BYTE_OFFSET	    0
+#define PL_RESET_ENABLE 		    1
+#define PL_RESET_DISABLE		    0
+
+/* Set observer key packet */
+#define PL_OBSERVER_KEY_OFFSET      0
+#define PL_OBSERVER_KEY_SIZE        32
+
+/* Network packet  */
+#define PL_NETWORK_MODE_OFFSET      0
+#define PL_NETWORK_MODE_STA         0
+#define PL_NETWORK_MODE_AP          1
+
+#define PL_NETWORK_PHY_OFFSET       1
+#define PL_NETWORK_PHY_B            0
+#define PL_NETWORK_PHY_G            1
+#define PL_NETWORK_PHY_N            2
+
+#define PL_NETWORK_CHANNEL_OFFSET   2
+#define PL_NETWORK_SSID_LEN_OFFSET  3
+
+/* USART packet */
+#define PL_USART_BAUDRATE_BYTE_OFFSET		(0)
+#define PL_USART_PARITY_BYTE_OFFSET		    (USART_BAUDRATE_BYTE_OFFSET+1)
+#define PL_USART_DATA_BITS_BYTE_OFFSET		(USART_PARITY_BYTE_OFFSET+1)
+#define PL_USART_STOP_BITS_BYTE_OFFSET		(USART_DATA_BITS_BYTE_OFFSET+1)
+
+/* USART configuration bytes */
+#define PL_USART_PARITY_EVEN		0xC0
+#define PL_USART_PARITY_ODD		    0xC1
+#define PL_USART_PARITY_NONE		0xC2
+#define PL_USART_DATA_BITS_8		0xC3
+#define PL_USART_DATA_BITS_9		0xC4
+#define PL_USART_STOP_BITS_1		0xC5
+#define PL_USART_STOP_BITS_2		0xC6
+#define PL_USART_BAUDRATE_9600		0xC7
+#define PL_USART_BAUDRATE_19200	    0xC8
+#define PL_USART_BAUDRATE_59600	    0xC9
+#define PL_USART_BAUDRATE_115200	0xCA
+
+/* USART errors */
+#define USART_PARITY_ERROR_BYTE 	0xE0
+#define USART_FRAME_ERROR_BYTE		0xE1
+#define USART_DATA_BITS_ERROR_BYTE	0xE2
+#define USART_STOP_BITS_ERROR_BYTE	0xE3
+#define USART_BAUDRATE_ERROR_BYTE	0xE4
+
+// #pragma GCC diagnostic ignored "-Wunused-function"
+
+
+#endif // PROTOCOL_H_INCLUDED
