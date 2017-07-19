9a52a41bf00d24c51a12f7fb8e3a9df25ec4372a
kripton <kamnev.u1969@gmail.com>
2017-07-19 08:25:46 +0300

    Initial programmer interface

---
+++ programmer.c
@@ -0,0 +1,76 @@
+#include "programmer.h"
+#include "osi.h"
+
+#include "spi.h"
+#include "config.h"
+
+
+/* Current SPI bitrate */
+static int spi_bitrate = PROG_SPI_DEFAULT_FREQ;
+
+/* Used to resume thread */
+static OsiSyncObj_t resume_obj;
+
+/* Task prototype */
+static void prog_task(void *pvParameters);
+
+
+static _i16 enable_pgm_mode(void) {
+}
+
+
+static inline void spi_enable(void) {
+    MAP_SPIReset(PROG_SPI_BASE);
+
+    MAP_SPIConfigSetExpClk(PROG_SPI_BASE,
+                           MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                           spi_bitrate,
+                           SPI_MODE_MASTER,
+                           SPI_SUB_MODE_0,
+                           (SPI_SW_CTRL_CS |
+                            SPI_4PIN_MODE |
+                            SPI_TURBO_OFF |
+                            SPI_CS_ACTIVELOW |
+                            SPI_WL_8));
+
+    MAP_SPIEnable(PROG_SPI_BASE);
+}
+
+
+static inline void spi_disable(void) {
+    MAP_SPIDisable(PROG_SPI_BASE);
+}
+
+
+_i16 programmer_start(void) {
+    _i16 status;
+
+    status = osi_SyncObjCreate(&resume_obj);
+    OSI_ASSERT_ON_ERROR(status);
+
+    status = osi_TaskCreate(prog_task, PROGRAMMER_TASK_NAME, PROGRAMMER_TASK_STACK_SIZE,
+                            NULL, PROGRAMMER_TASK_PRIO, NULL);
+    OSI_ASSERT_ON_ERROR(status);
+
+    return status;
+}
+
+
+_i16 programmer_resume(OsiMsgQ_t *in_queue, OsiMsgQ_t *out_queu) {
+    _i16 status;
+
+    status = osi_SyncObjSignal(&resume_obj);
+    OSI_ASSERT_ON_ERROR(status);
+
+    return status;
+}
+
+
+static void prog_task(void *pvParameters) {
+prog_entry:
+    osi_SyncObjWait(&resume_obj, OSI_WAIT_FOREVER);
+
+    /* Here comes the action */
+    spi_enable();
+}
+
---
+++ programmer.h
@@ -0,0 +1,12 @@
+#ifndef PROGRAMMER_H_INCLUDED
+#define PROGRAMMER_H_INCLUDED
+
+#include "osi.h"
+
+
+typedef {
+
+}ProgrammerConfig;
+
+
+#endif // PROGRAMMER_H_INCLUDED
