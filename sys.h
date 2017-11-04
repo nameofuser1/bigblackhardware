815021fe4048c997c02bab410f60f2dd45266a24
kripton <kamnev.u1969@gmail.com>
2017-11-04 22:00:29 +0300

    Critical error handler

--- sys.h
+++ sys.h
@@ -3,11 +3,19 @@
 
 #include "simplelink.h"
 #include "osi.h"
+#include "logging.h"
+
 
 void sys_restart(void);
 _i16 sys_queue_read_ptr(OsiMsgQ_t *queue, void **ptr, OsiTime_t timeout);
 _i16 sys_queue_write_ptr(OsiMsgQ_t *queue, void *ptr, OsiTime_t timeout);
 
+
+#define SYS_ASSERT_CRITICAL(status)     OSI_ASSERT_WITHOUT_EXIT(status);\
+                                        if(status < 0) {\
+                                            OSI_COMMON_LOG("CRITICAL ERROR. RESTARTING\r\n");\
+                                            sys_restart();\
+                                        }
 
 #define MCU_RESET_ON    0
 #define MCU_RESET_OFF   1
